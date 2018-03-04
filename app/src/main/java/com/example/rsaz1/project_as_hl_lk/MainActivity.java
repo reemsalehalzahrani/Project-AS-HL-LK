b4c9f09bfa59a1976676ebe935c087ecf3626de6
reem.alzhrani <reem.alzahrani@labsystems.sa>
2018-03-04 12:41:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/rsaz1/project_as_hl_lk/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rsaz1.project_as_hl_lk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
